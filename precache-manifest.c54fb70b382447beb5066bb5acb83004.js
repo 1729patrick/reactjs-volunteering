a45975c9a707104ec14d5c45c1e23b987e8fd403
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-06-04 21:54:10 +0100

    gh pages

---
+++ precache-manifest.c54fb70b382447beb5066bb5acb83004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7170ae9786946cb0eb5d167fae956a58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda7f3207eaf2629024b",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "bda7f3207eaf2629024b",
+    "url": "/static/js/2.f22ff67c.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.f22ff67c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc3b09a47fd0088cd2b",
+    "url": "/static/js/main.f7f7305f.chunk.js"
+  },
+  {
+    "revision": "798f76c80dcadeb6cc12",
+    "url": "/static/js/runtime-main.5d63068a.js"
+  }
+]);
--- src/pages/Config/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// import { Container } from './styles';
-
-function Config() {
-  return <div />;
-}
-
-export default Config;
