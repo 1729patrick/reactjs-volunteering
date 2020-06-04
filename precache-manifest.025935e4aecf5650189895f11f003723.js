39193f8df3bcf79cf2f9fe810220cef0f2054913
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-06-04 22:03:00 +0100

    Updates

---
+++ precache-manifest.025935e4aecf5650189895f11f003723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d881c55996a1a14e1eb989ad1e829f",
+    "url": "/reactjs-volunteering/index.html"
+  },
+  {
+    "revision": "f3a9c47ca87b015ad6a1",
+    "url": "/reactjs-volunteering/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "f3a9c47ca87b015ad6a1",
+    "url": "/reactjs-volunteering/static/js/2.74661a58.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/reactjs-volunteering/static/js/2.74661a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc3b09a47fd0088cd2b",
+    "url": "/reactjs-volunteering/static/js/main.f7f7305f.chunk.js"
+  },
+  {
+    "revision": "338d76d24a4b6759b85d",
+    "url": "/reactjs-volunteering/static/js/runtime-main.a7175f19.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c54fb70b382447beb5066bb5acb83004.js"
+  "/reactjs-volunteering/precache-manifest.025935e4aecf5650189895f11f003723.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactjs-volunteering/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
